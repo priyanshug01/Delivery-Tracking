843df9e767a0bb8a14472c206006c574f07cf32f
priyanshug01 <priyanshu945pg@gmail.com>
2023-08-17 14:00:14 +0530

    Updated Images Look

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../styles/navbar.css';
+import logo1 from '../images/collage.png';
+import logo2 from '../images/place.png';
+import logo3 from '../images/delivery.png';
+import logo4 from '../images/car.png';
+import logo5 from '../images/user.png';
+import logo6 from '../images/settings.png';
 
 const NavBar = () => {
   return (
@@ -7,12 +13,12 @@ const NavBar = () => {
       <div className="navbar">
         <nav className="nav-links">
           <ul>
-            <li><img src="" alt="D" /></li>
-            <li><img src="" alt="L" /></li>
-            <li><img src="" alt="T" /></li>
-            <li><img src="" alt="C" /></li>
-            <li><img src="" alt="A" /></li>
-            <li><img src="" alt="S" /></li>
+            <li><img src={logo1} alt="D" /></li>
+            <li><img src={logo2} alt="L" /></li>
+            <li><img src={logo3} alt="T" /></li>
+            <li><img src={logo4} alt="C" /></li>
+            <li><img src={logo5} alt="A" /></li>
+            <li><img src={logo6} alt="S" /></li>
           </ul>
         </nav>
       </div>
--- src/components/tracking.js
+++ src/components/tracking.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/tracking.css';
+import man from '../images/man.png';
 
 const Tracking = () => {
-    const userImage = "";
     return (
         <>
             <div className="heading">Tracking</div>
             <div className="user">
-                <img src={userImage} alt="user" />
+                <img src={man} alt="user" />
                 <span className='username'>Mathew Perry</span>
             </div>
             <div className='tracking-container'></div>

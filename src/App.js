44b6c7ea87a5f4697a1828566aba68eb86eb1168
priyanshug01 <priyanshu945pg@gmail.com>
2023-08-23 22:42:50 +0530

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Tracking from './components/tracking';
+import Account from './components/account';
 
 function App() {
   return (
     <div className="App">
-      <Tracking />
+      <Account />
     </div>
   );
 }
--- src/components/account.js
+++ src/components/account.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import '../styles/account.css';
 
@@ -14,31 +13,29 @@ const Account = () => {
   };
 
   return (
-    <div className="container">
-      <div className="back">
-        <div className="box signin">
-          <h2>Already Have an Account?</h2>
-          <button className="signinBtn" onClick={handleSigninClick}>Sign In</button>
+    <div className={`acc-page ${activeForm === 'signup' ? 'active' : ''}`}>
+      <div className={`acc-container ${activeForm === 'signup' ? 'active' : ''}`}>
+        <div className="backg">
+          <div className="acc-box signin">
+            <h2>Already Have an Account?</h2>
+            <button className="signinBtn" onClick={handleSigninClick}>Sign In</button>
+          </div>
+          <div className="acc-box signup">
+            <h2>Don't Have an Account?</h2>
+            <button className="signupBtn" onClick={handleSignupClick}>Sign Up</button>
+          </div>
         </div>
-        <div className="box signup">
-          <h2>Don't Have an Account?</h2>
-          <button className="signupBtn" onClick={handleSignupClick}>Sign Up</button>
-        </div>
-      </div>
-      <div className="formA">
-        {activeForm === 'signin' && (
-          <div className="form signinForm">
+        <div className={`acc-form ${activeForm === 'signup' ? 'active' : ''}`}>
+          <div className={`form signinForm ${activeForm === 'signin' ? 'active' : ''}`}>
             <form>
               <h3>Sign In</h3>
               <input type="text" placeholder="Username" />
               <input type="password" placeholder="Password" />
               <input type="submit" value="Login" />
-              <a href="#" className="forgot">Forgot Password?</a>
+              <a href="#" className="forget">Forgot Password?</a>
             </form>
           </div>
-        )}
-        {activeForm === 'signup' && (
-          <div className="form signupForm">
+          <div className={`form signupForm ${activeForm === 'signup' ? 'active' : ''}`}>
             <form>
               <h3>Sign Up</h3>
               <input type="text" placeholder="Username" />
@@ -48,7 +45,7 @@ const Account = () => {
               <input type="submit" value="Sign Up" />
             </form>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );

e2d0d341fde923491a9a2c324fb78fdf595c4c8c
priyanshug01 <priyanshu945pg@gmail.com>
2023-08-23 23:12:43 +0530

    Changed Layout

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/account.css';
+import NavBar from '../components/navbar';
 
 const Account = () => {
   const [activeForm, setActiveForm] = useState('signin');
@@ -13,41 +14,44 @@ const Account = () => {
   };
 
   return (
-    <div className={`acc-page ${activeForm === 'signup' ? 'active' : ''}`}>
-      <div className={`acc-container ${activeForm === 'signup' ? 'active' : ''}`}>
-        <div className="backg">
-          <div className="acc-box signin">
-            <h2>Already Have an Account?</h2>
-            <button className="signinBtn" onClick={handleSigninClick}>Sign In</button>
+    <>
+    <NavBar />
+      <div className={`acc-page ${activeForm === 'signup' ? 'active' : ''}`}>
+        <div className={`acc-container ${activeForm === 'signup' ? 'active' : ''}`}>
+          <div className="backg">
+            <div className="acc-box signin">
+              <h2>Already Have an Account?</h2>
+              <button className="signinBtn" onClick={handleSigninClick}>Sign In</button>
+            </div>
+            <div className="acc-box signup">
+              <h2>Don't Have an Account?</h2>
+              <button className="signupBtn" onClick={handleSignupClick}>Sign Up</button>
+            </div>
           </div>
-          <div className="acc-box signup">
-            <h2>Don't Have an Account?</h2>
-            <button className="signupBtn" onClick={handleSignupClick}>Sign Up</button>
-          </div>
-        </div>
-        <div className={`acc-form ${activeForm === 'signup' ? 'active' : ''}`}>
-          <div className={`form signinForm ${activeForm === 'signin' ? 'active' : ''}`}>
-            <form>
-              <h3>Sign In</h3>
-              <input type="text" placeholder="Username" />
-              <input type="password" placeholder="Password" />
-              <input type="submit" value="Login" />
-              <a href="#" className="forget">Forgot Password?</a>
-            </form>
-          </div>
-          <div className={`form signupForm ${activeForm === 'signup' ? 'active' : ''}`}>
-            <form>
-              <h3>Sign Up</h3>
-              <input type="text" placeholder="Username" />
-              <input type="email" placeholder="Email Address" />
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Confirm Password" />
-              <input type="submit" value="Sign Up" />
-            </form>
+          <div className={`acc-form ${activeForm === 'signup' ? 'active' : ''}`}>
+            <div className={`form signinForm ${activeForm === 'signin' ? 'active' : ''}`}>
+              <form>
+                <h3>Sign In</h3>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <input type="submit" value="Login" />
+                <a href="#" className="forget">Forgot Password?</a>
+              </form>
+            </div>
+            <div className={`form signupForm ${activeForm === 'signup' ? 'active' : ''}`}>
+              <form>
+                <h3>Sign Up</h3>
+                <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email Address" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Confirm Password" />
+                <input type="submit" value="Sign Up" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

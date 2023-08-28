af2dedd4dec5e5414386cd287f2e9e4154cb261e
priyanshug01 <priyanshu945pg@gmail.com>
2023-08-29 00:31:57 +0530

    Major Updates

--- src/pages/location.js
+++ src/pages/location.js
@@ -1,17 +1,148 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/location.css';
 import NavBar from '../components/navbar';
-import man from '../images/man.png';
+import upArrow from '../images/upArrow.png';
+import downArrow from '../images/downArrow.png';
 
 const Location = () => {
+    const colHeadings1 = ["Trip ID", "Client Name", "Supplier Name", "Booking Date"];
+    const colHeadings2 = ["POC Name", "POC Number", "Vehicle Placed By", "Status"];
+    const colHeadings3 = ["Vehicle Type", "Vehicle Number", "Driver Name", "Driver Number"];
+    const colHeadings4 = ["Origin", "Destination", "Fare Amount", "Add Attachments"];
+
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [isFormVisible, setIsFormVisible] = useState(true);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
+
+    const handleDropdownChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedOptions(selectedValue);
+    };
+
+    const toggleFormVisibility = () => {
+        setIsFormVisible(prevVisible => !prevVisible);
+    };
+
     return (
         <>
             <NavBar />
-            <div className="heading">Location</div>
-            <div className="user">
-                <img src={man} alt="user" />
-                <span className='username'>Mathew Perry</span>
+            <div className="heading">Entry Field
+                <button className="toggle-button" onClick={toggleFormVisibility}>
+                    <img src={isFormVisible ? upArrow : downArrow} alt="Toggle Arrow" />
+                </button>
             </div>
+
+            {isFormVisible && (
+                <div className="entry-table-container">
+                    <table className="entry-table">
+                        <thead>
+                            <tr>
+                                {colHeadings1.map((colHeading, index) => (
+                                    <th key={index}>{colHeading}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {Array.from({ length: 4 }).map((_, colIndex) => (
+                                    <td key={colIndex}>
+                                        {colIndex === 3 ? (
+                                            <input type="date" />
+                                        ) : (
+                                            <input type="text" placeholder={`Enter ${colHeadings1[colIndex]}`} />
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="entry-table">
+                        <thead>
+                            <tr>
+                                {colHeadings2.map((colHeading, index) => (
+                                    <th key={index}>{colHeading}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {Array.from({ length: 4 }).map((_, colIndex) => (
+                                    <td key={colIndex}>
+                                        {colIndex === 3 ? (
+                                            <select className="entry-dropdown" value={selectedOptions} onChange={handleDropdownChange}>
+                                                <option value="Shipped">Shipped</option>
+                                                <option value="Delivered">Delivered</option>
+                                                <option value="Cancelled">Cancelled</option>
+                                                <option value="Pending">Pending</option>
+                                            </select>
+                                        ) : (
+                                            <input type="text" placeholder={`Enter ${colHeadings2[colIndex]}`} />
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="entry-table">
+                        <thead>
+                            <tr>
+                                {colHeadings3.map((colHeading, index) => (
+                                    <th key={index}>{colHeading}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {Array.from({ length: 4 }).map((_, colIndex) => (
+                                    <td key={colIndex}>
+                                        <input type="text" placeholder={`Enter ${colHeadings3[colIndex]}`} />
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="entry-table">
+                        <thead>
+                            <tr>
+                                {colHeadings4.map((colHeading, index) => (
+                                    <th key={index}>{colHeading}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {Array.from({ length: 4 }).map((_, colIndex) => (
+                                    <td key={colIndex}>
+                                        {colIndex === 3 ? (
+                                            <div className="upload-container">
+                                                <input
+                                                    className='custom-file-input'
+                                                    type="file"
+                                                    accept=".pdf,.doc,.docx,.jpg,.png"
+                                                    onChange={handleFileChange}
+                                                />
+                                                {selectedFile && (
+                                                    <div className="selected-file">
+                                                        Selected File: {selectedFile.name}
+                                                    </div>
+                                                )}
+                                            </div>
+                                        ) : (
+                                            <input type="text" placeholder={`Enter ${colHeadings4[colIndex]}`} />
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button className="entry-button" type="button">Add New Details</button>
+                </div>
+            )}
         </>
     );
 };
--- src/pages/settings.js
+++ src/pages/settings.js
@@ -3,9 +3,15 @@ import '../styles/settings.css';
 import NavBar from '../components/navbar';
 
 const Settings = () => {
-
-    const [inputValues, setInputValues] = useState(['', '', '', '']);
-    const [selectedOption, setSelectedOption] = useState('Option 1');
+    const [inputValues, setInputValues] = useState(['', '', '', '', '', '', '']);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [tableData, setTableData] = useState([
+        { id: 1, tid: '#12489-DL-NY', date: '01-04-2023', suppliername: 'GOODWILL INDIA TRANS AND LOGISTICS PVT LTD', pocname: 'Nikhil', pocno: '9868856732', route: 'Noida - Tirupati', vehtype: '32FT SXL 7MT', status: 'Delivered' },
+        { id: 2, tid: '#12489-DL-NY', date: '02-04-2023', suppliername: 'NEW AZAD TRANSPORT SERVICE', pocname: 'Sonu', pocno: '9868856732', route: 'Mundka - Guhawati', vehtype: '32FT MXL 15MT', status: 'Pending' },
+        { id: 3, tid: '#12489-DL-NY', date: '04-04-2023', suppliername: 'NDCC CARGO CARRIERS', pocname: 'Bharat', pocno: '9868856732', route: 'Alwar - Kolkata', vehtype: '32FT MXL 17.5MT', status: 'Delivered' },
+        { id: 4, tid: '#12489-DL-NY', date: '04-04-2023', suppliername: 'RAHUL ROADLINE', pocname: 'Sandeep', pocno: '9868856732', route: 'Sonipat - Guhawati', vehtype: '32FT SXL 7MT', status: 'Delivered' },
+        { id: 5, tid: '#12489-DL-NY', date: '05-04-2023', suppliername: 'NDCC CARGO CARRIERS', pocname: 'Aman', pocno: '9868856732', route: 'Alwar - Guhawati', vehtype: '32FT MXL 17.5MT', status: 'Shipped' },
+    ]);
 
     const handleInputChange = (index, event) => {
         const newInputValues = [...inputValues];
@@ -14,28 +20,47 @@ const Settings = () => {
     };
 
     const handleDropdownChange = (event) => {
-        setSelectedOption(event.target.value);
+        const selectedValue = event.target.value;
+        setSelectedOptions(selectedValue);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('Submitted values:', inputValues);
-        console.log('Selected option:', selectedOption);
+
+        const newId = tableData.length + 1;
+        const newTid = inputValues[0];
+        const newFromDate = inputValues[1];
+        const newSuppName = inputValues[2];
+        const newPocName = inputValues[3];
+        const newPocNo = inputValues[4];
+        const newRoute = inputValues[5];
+        const newVehType = inputValues[6];
+        const newStatus = selectedOptions;
+
+        const newRow = {
+            id: newId,
+            tid: newTid,
+            date: newFromDate,
+            suppliername: newSuppName,
+            pocname: newPocName,
+            pocno: newPocNo,
+            route: newRoute,
+            vehtype: newVehType,
+            status: newStatus,
+        };
+
+        setTableData([...tableData, newRow]);
+        setInputValues(['', '', '', '', '', '', '']);
+        setSelectedOptions([]);
     };
-
-    const inputHeaders = ['From', 'To', 'From', 'To'];
-
-    const [data] = useState([
-        { id: 1, tid: '#127777489-DL-NY', date: '01-04-2023', origin: 'Noida', destination: 'Tirupati', status: 'Delivered' },
-        { id: 2, tid: '#127777489-DL-NY', date: '02-04-2023', origin: 'Mundka', destination: 'Guhawati', status: 'Pending' },
-        { id: 3, tid: '#127777489-DL-NY', date: '04-04-2023', origin: 'Alwar', destination: 'Kolkata', status: 'Delivered' },
-        { id: 4, tid: '#127777489-DL-NY', date: '04-04-2023', origin: 'Sonipat', destination: 'Guhawati', status: 'Delivered' },
-        { id: 5, tid: '#127777489-DL-NY', date: '05-04-2023', origin: 'Alwar', destination: 'Guhawati', status: 'Shipped' },
-    ]);
 
     const handleView = (id) => {
-        window.open('./details', '_blank');
+        // window.open(`./details/${id}`, '_blank');
+        window.open(`./details`, '_blank');
+
     };
+
+    const inputHeaders = ['Trip ID', 'Booking Date', 'Supplier Name', 'POC Name', 'POC Number', 'Route', 'Vehicle Type'];
 
     return (
         <>
@@ -46,7 +71,7 @@ const Settings = () => {
                     {inputValues.map((value, index) => (
                         <div key={index} className="input-field">
                             <h3 className="input-header">{inputHeaders[index]}</h3>
-                            {index === 2 || index === 3 ? (
+                            {index === 1 ? (
                                 <input
                                     className="form-input"
                                     type="date"
@@ -59,21 +84,21 @@ const Settings = () => {
                                     type="text"
                                     value={value}
                                     onChange={(event) => handleInputChange(index, event)}
-                                    placeholder={`Enter Location`}
+                                    placeholder={`Enter ${inputHeaders[index]}`}
                                 />
                             )}
                         </div>
                     ))}
                     <div className="dropdown-container">
                         <h3 className="dropdown-header">Status</h3>
-                        <select className="dropdown" value={selectedOption} onChange={handleDropdownChange}>
-                            <option value="Option 1">Shipped</option>
-                            <option value="Option 2">Delivered</option>
-                            <option value="Option 3">Cancelled</option>
-                            <option value="Option 2">Pending</option>
+                        <select className="dropdown" value={selectedOptions} onChange={handleDropdownChange}>
+                            <option value="Shipped">Shipped</option>
+                            <option value="Delivered">Delivered</option>
+                            <option value="Cancelled">Cancelled</option>
+                            <option value="Pending">Pending</option>
                         </select>
                     </div>
-                    <button className="form-button" type="submit">Submit</button>
+                    <button className="form-button" type="submit">Add New Details</button>
                 </div>
             </form>
 
@@ -81,21 +106,27 @@ const Settings = () => {
                 <table className="table-data">
                     <thead>
                         <tr>
-                            <th>Transaction ID</th>
+                            <th>Trip ID</th>
                             <th>Booking Date</th>
-                            <th>Origin</th>
-                            <th>Destination</th>
+                            <th>Supplier Name</th>
+                            <th>POC Name</th>
+                            <th>POC Number</th>
+                            <th>Route</th>
+                            <th>Vehicle Type</th>
                             <th>Status</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map(item => (
+                        {tableData.map(item => (
                             <tr key={item.id}>
                                 <td>{item.tid}</td>
                                 <td>{item.date}</td>
-                                <td>{item.origin}</td>
-                                <td>{item.destination}</td>
+                                <td>{item.suppliername}</td>
+                                <td>{item.pocname}</td>
+                                <td>{item.pocno}</td>
+                                <td>{item.route}</td>
+                                <td>{item.vehtype}</td>
                                 <td>{item.status}</td>
                                 <td><button className="view-btn" onClick={() => handleView(item.id)}>
                                     View
